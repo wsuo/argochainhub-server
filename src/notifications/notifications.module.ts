b997ebe893fc8d7eb8f2072580693c15ef329963
wangshuo <wangsuoo@qq.com>
2025-08-06 15:52:07 +0800

    更新通知模块配置
    - 在NotificationsModule中注册管理员通知相关服务
    - 添加AdminNotificationsService和AdminNotificationsController
    - 集成SystemMonitorService系统监控服务
    - 配置定时任务支持和数据库实体
    - 完善模块依赖关系和服务导出

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,16 +2,34 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { Notification } from '../entities/notification.entity';
+import { AdminNotification } from '../entities/admin-notification.entity';
 import { User } from '../entities/user.entity';
+import { AdminUser } from '../entities/admin-user.entity';
+import { Company } from '../entities/company.entity';
+import { Product } from '../entities/product.entity';
+import { Inquiry } from '../entities/inquiry.entity';
 import { NotificationsController } from './notifications.controller';
+import { AdminNotificationsController } from './admin-notifications.controller';
+import { SystemMonitorController } from './system-monitor.controller';
 import { NotificationsService } from './notifications.service';
+import { AdminNotificationsService } from './admin-notifications.service';
+import { SystemMonitorService } from './system-monitor.service';
 import { EventsService } from './events.service';
 import { NotificationGateway } from './notification.gateway';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Notification, User]),
+    TypeOrmModule.forFeature([
+      Notification, 
+      AdminNotification, 
+      User, 
+      AdminUser, 
+      Company, 
+      Product, 
+      Inquiry
+    ]),
     JwtModule.registerAsync({
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('jwt.secret') || 'default-secret',
@@ -19,9 +37,26 @@ import { NotificationGateway } from './notification.gateway';
       }),
       inject: [ConfigService],
     }),
+    ScheduleModule.forRoot(), // 启用定时任务
   ],
-  controllers: [NotificationsController],
-  providers: [NotificationsService, EventsService, NotificationGateway],
-  exports: [NotificationsService, EventsService, NotificationGateway],
+  controllers: [
+    NotificationsController, 
+    AdminNotificationsController, 
+    SystemMonitorController
+  ],
+  providers: [
+    NotificationsService, 
+    AdminNotificationsService, 
+    SystemMonitorService,
+    EventsService, 
+    NotificationGateway
+  ],
+  exports: [
+    NotificationsService, 
+    AdminNotificationsService, 
+    SystemMonitorService,
+    EventsService, 
+    NotificationGateway
+  ],
 })
 export class NotificationsModule {}
