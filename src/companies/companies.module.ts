100165bdda370f6ff20aa4b6d5a3be519f395d16
wangshuo <wangsuoo@qq.com>
2025-08-14 20:43:28 +0800

    feat: 集成供应商收藏功能到公司模块
    - 注册SupplierFavorite实体到TypeORM
    - 添加SupplierFavoritesService和Controller到模块
    - 完善模块依赖和导出配置

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -3,15 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from '../entities/company.entity';
 import { User } from '../entities/user.entity';
 import { Subscription } from '../entities/subscription.entity';
+import { SupplierFavorite } from '../entities/supplier-favorite.entity';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 import { CompanyUsersService } from './company-users.service';
 import { CompanyUsersController } from './company-users.controller';
+import { SupplierFavoritesService } from './supplier-favorites.service';
+import { SupplierFavoritesController } from './supplier-favorites.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company, User, Subscription])],
-  providers: [CompaniesService, CompanyUsersService],
-  controllers: [CompaniesController, CompanyUsersController],
-  exports: [CompaniesService, CompanyUsersService],
+  imports: [TypeOrmModule.forFeature([Company, User, Subscription, SupplierFavorite])],
+  providers: [CompaniesService, CompanyUsersService, SupplierFavoritesService],
+  controllers: [CompaniesController, CompanyUsersController, SupplierFavoritesController],
+  exports: [CompaniesService, CompanyUsersService, SupplierFavoritesService],
 })
 export class CompaniesModule {}
